d6733db0e94625c17f043e0263918fe396572a80
Preetam Pati <preetam6teen@gmail.com>
2023-11-30 07:40:04 +0530

    Added word finder via any filter - Preetam Pati

--- Playground/Utils/word_finder.py
+++ Playground/Utils/word_finder.py
@@ -1,12 +1,22 @@
+from . import MongoClient
 
+def word_finder(form_id, target_word, filter, filter_val):
 
-def word_finder(responses, word):
-    target_customers = list()
-    for response in responses:
-        if word in response["response"].split():
-            target_customers.append(response["customer-id"])
-    if len(target_customers) > 0:
-        return (
-            False, target_customers
-        )
-    return True, target_customers
+    queries = list(MongoClient.find({"type": "query", "form-id": form_id}))
+    customer_ids = list()
+    for query in queries:
+        responses = MongoClient.find({"type": "response", "query-id": query["id"]})
+        for response in responses:
+            if response["customer-id"] not in customer_ids:
+                customer_ids.append(response["customer-id"])
+    for customer_id in customer_ids:
+        for query in queries:
+            customer = MongoClient.find_one({"type": "customer", "id": customer_id})
+            if customer[filter] == filter_val:
+                responses = list(MongoClient.find({"type": "response", "customer-id": customer_id, "query-id": query["id"]}))
+                if len(responses) > 0:
+                    words = responses[0]["response"].split()
+                    if target_word in words:
+                        print(f"Offensive Data Entries: Customer Id {customer_id}")
+
+# word_finder("3abe191c-aedd-4b79-9378-e4fca037492b", "Faltu", "location", "Kolkata")
